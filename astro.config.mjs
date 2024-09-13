35a86c1b9ec721e911ccc4a09d64e656f214e2ff
haydonge <haydonge@gmail.com>
2024-09-13 14:47:12 +0800

    add cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import icon from "astro-icon";
 import tailwind from "@astrojs/tailwind";
 // import netlify from "@astrojs/netlify";
 import db from "@astrojs/db";
-//import cloudflare from "@astrojs/cloudflare";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
